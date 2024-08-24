5c8789b36ee8b54b56eb38ea0b05814cda4eec85
LKiraAhmed <lkiraww2@gmail.com>
2024-08-23 22:06:38 -0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>L-api</title>
+</head>
+<body>
+    <div>   
+        <h1 style="background-color: aqua"> HELLO  please You should put these jumping words use it   </h1>
+        <p>
+            An information that puts a jump every / after a sentence you put an id.
+            <br>Cappy :<br>
+            customer:<br>   
+            <ul>
+                <li>customer</li>
+                <li>createcustomer</li>
+                <li>updatecustomer/</li>
+                <li>customer/</li>
+                <li>delete/</li>
+            </ul>
+            invoice:<br>   
+            <ul>
+                <li>invoice</li>
+                <li>createinvoice</li>
+                <li>updateinvoice/</li>
+                <li>deleteinvoice/</li>
+            </ul>
+            auth:<br>   
+            <ul>
+                <li>createacuont</li>
+                <li>loginaccount</li>
+            </ul>
+        </p>
+    </div>
+</body>
+</html>
